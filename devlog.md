855e0e310cc1b7a4481810a021b652c1452f44e6
Poojith Nukala <nukala180@gmail.com>
2024-12-08 17:34:47 -0600

    12/8/2024 5:01OPM

--- devlog.md
+++ devlog.md
@@ -9,4 +9,90 @@ Created the Github
 I didn't read all the way through the instructions and didn't see the part about the github til now. I created the Repo and created the original devlog file on the github page. I worked on the logic behind all the commands (create, open, insert,search,load, print, extract, quit). I'm currently having issues with the insert, search, load and extract as they have to be developed as im creating the index file and B-tree implemetation. I tried pushing into the repo and I was running into some issues with "main" branch. I googled the issues and saw that there is a git push force command and I used this command to push my code. However, this deleted my original devlog file and pushed the code into main branch. I think from now on, I'm going to create another branch and then merge it to the main branch. I attemped my regular push to main and it works now?? I don't know what happened originally but it's working now. No need to do pull and merges.
 
 12/8/2024 1:28PM
+
 Started working on the B-Tree Structure. I have to create the B-Tree node class which includes the Block ID, Parent block ID, num of key value pairs, and array of keys, values, and child pointers. I also have to create methods to read and write a node from or to the index file. An issue I ran into while implementing the B-Tree class was the commands weren't printing properly. The insert command doesn't store the key value pair in a data structure, it just displays the input. This needs to be changed so the BTreeNode class stores the key value pairs in the B-Tree structure. The Seach command doesn't search the B-Tree, it just prints the input, so this needs to be changed also. The Print command doesn't print anything right now, it needs to print the stored key value pairs. The Extract command doesn't read and extract the key-value pairs, it's just null at the moment. So essentially, all the commands need to be changed so it works along the B-Tree.
+
+12/8/2024 5:01PM
+
+Finished the B-Tree Structure in the BTreeNode class. The __init__ constructor initialized a new BTreeNode object. It contained the parameters that stored the unique id of the block where this node resides in the file and the id of the parent node block. The serialize def converts the node into a binary format that can be written to a file. It essentially packs the attributes that were created in the __init__ constructor into a binary string using struct.pack. Then for each key value pair in the node, it adds the key and value to the binary data, using 8 bytes for each and adds the children array. This def returns a binary string representing the serialised node. The deserialize def creates a BTreeNode object from a binary block of data read from a file. It essentially reconstructs a node from a binary block of data read from a file, making it usable in memory. It returns a BTreeNode object reconstructed from the binary data. 
+
+Example Output:
+
+Commands: create, open, insert, search, load, print, extract, quit
+
+Enter a command: create
+
+Enter the name of the new index file: index.btree
+
+File 'index.btree' created and opened.
+
+
+Commands: create, open, insert, search, load, print, extract, quit
+
+Enter a command: insert
+
+Enter key (unsigned integer): 10
+
+Enter value (unsigned integer): 100
+
+Inserted key=10, value=100.
+
+
+Commands: create, open, insert, search, load, print, extract, quit
+Enter a command: insert
+
+Enter key (unsigned integer): 400
+
+Enter value (unsigned integer): 20
+
+Inserted key=400, value=20.
+
+
+Commands: create, open, insert, search, load, print, extract, quit
+
+Enter a command: search
+
+Enter key to search (unsigned integer): 400
+
+Search result: key=400, value=20.
+
+
+Commands: create, open, insert, search, load, print, extract, quit
+
+Enter a command: 10
+
+Invalid command.
+
+
+Commands: create, open, insert, search, load, print, extract, quit
+
+Enter a command: search
+
+Enter key to search (unsigned integer): 10
+
+Search result: key=10, value=100.
+
+
+Commands: create, open, insert, search, load, print, extract, quit
+
+Enter a command: print 
+
+Key: 10, Value: 100
+
+Key: 400, Value: 20
+
+
+Commands: create, open, insert, search, load, print, extract, quit
+
+Enter a command: extract
+
+Enter the file name: out.txt
+
+Extracted key/value pairs to 'out.txt'.
+
+
+Commands: create, open, insert, search, load, print, extract, quit
+
+Enter a command: quit
+
+Exiting program.
--- main.py
+++ main.py
@@ -1,6 +1,54 @@
 import os
 import struct
 
+class BTreeNode:
+    blockSize = 512
+    maxKey = 19
+    maxChild = 20
+
+    def __init__(self, id, parentId=0):
+        self.id = id
+        self.parentId = parentId
+        self.pairs = 0
+        self.keys = [0] * self.maxKey
+        self.values = [0] * self.maxKey
+        self.children = [0] * self.maxChild
+
+    def serialize(self):
+        data = struct.pack('>Q', self.id)
+        data += struct.pack('>Q', self.parentId)
+        data += struct.pack('>Q', self.pairs)
+        for i in range(self.pairs):
+            data += struct.pack('>Q', self.keys[i])
+            data += struct.pack('>Q', self.values[i])
+        for child in self.children:
+            data += struct.pack('>Q', child)
+        padding = self.blockSize - len(data)
+        data += b'\x00' * padding
+        return data
+
+    @classmethod
+    def deserialize(cls, data):
+        id = struct.unpack('>Q', data[0:8])[0]
+        parentId = struct.unpack('>Q', data[8:16])[0]
+        pairs = struct.unpack('>Q', data[16:24])[0]
+        offset = 24
+        keys = []
+        values = []
+        for i in range(pairs):
+            keys.append(struct.unpack('>Q', data[offset:offset + 8])[0])
+            offset += 8
+            values.append(struct.unpack('>Q', data[offset:offset + 8])[0])
+            offset += 8
+        children = [struct.unpack('>Q', data[offset + i * 8:offset + (i + 1) * 8])[0] for i in range(cls.maxChild)]
+        node = cls(id, parentId)
+        node.pairs = pairs
+        node.keys[:pairs] = keys
+        node.values[:pairs] = values
+        node.children = children
+        return node
+
+
 class BTreeIndex:
     def __init__(self):
         self.currentFile = None
@@ -14,8 +62,9 @@ class BTreeIndex:
                 print("Quitting")
                 return
         with open(name, 'wb') as f:
-            f.write(struct.pack('>Q', 0))  
-            f.write(struct.pack('>Q', 1)) 
+            f.write(b'4337PRJ3')
+            f.write(struct.pack('>Q', 0))
+            f.write(struct.pack('>Q', 1))
         self.filePath = name
         self.currentFile = open(self.filePath, 'r+b')
         print(f"File '{name}' created and opened.")
@@ -26,7 +75,7 @@ class BTreeIndex:
             print("File does not exist.")
             return
         with open(name, 'rb') as f:
-            header = f.read(16)  
+            header = f.read(16)
             if len(header) < 16:
                 print("Invalid file format.")
                 return
@@ -41,9 +90,49 @@ class BTreeIndex:
         try:
             key = int(input("Enter key (unsigned integer): "))
             value = int(input("Enter value (unsigned integer): "))
+            self.currentFile.seek(0)
+            header = self.currentFile.read(24)
+            rootId = struct.unpack('>Q', header[8:16])[0]
+            if rootId == 0:
+                rootNode = BTreeNode(id=1)
+                rootNode.keys[0] = key
+                rootNode.values[0] = value
+                rootNode.pairs = 1
+                self.currentFile.seek(512)
+                self.currentFile.write(rootNode.serialize())
+                self.currentFile.seek(8)
+                self.currentFile.write(struct.pack('>Q', 1))
+            else:
+                self.insertToBtree(rootId, key, value)
             print(f"Inserted key={key}, value={value}.")
         except ValueError:
             print("Invalid input.")
+
+    def insertToBtree(self, id, key, value):
+        self.currentFile.seek(id * 512)
+        data = self.currentFile.read(512)
+        node = BTreeNode.deserialize(data)
+
+        i = 0
+        while i < node.pairs and key > node.keys[i]:
+            i += 1
+
+        if i < node.pairs and node.keys[i] == key:
+            print(f"Error: Key {key} already exists.")
+            return
+
+        if node.pairs < BTreeNode.maxKey:
+            node.keys.insert(i, key)
+            node.values.insert(i, value)
+            node.keys.pop()
+            node.values.pop()
+            node.pairs += 1
+
+            self.currentFile.seek(id * 512)
+            self.currentFile.write(node.serialize())
+        else:
+            print("Node splitting required (not implemented).")
+
 
     def search(self):
         if not self.currentFile:
@@ -51,9 +140,32 @@ class BTreeIndex:
             return
         try:
             key = int(input("Enter key to search (unsigned integer): "))
-            print(f"Search result: key={key}, value=<value>.")
+            self.currentFile.seek(0)
+            header = self.currentFile.read(24)
+            rootId = struct.unpack('>Q', header[8:16])[0]
+            if rootId == 0:
+                print("Tree is empty.")
+                return
+            value = self.searchBtree(rootId, key)
+            if value is not None:
+                print(f"Search result: key={key}, value={value}.")
+            else:
+                print(f"Key {key} not found.")
         except ValueError:
             print("Invalid input.")
+
+    def searchBtree(self, id, key):
+        self.currentFile.seek(id * 512)
+        data = self.currentFile.read(512)
+        node = BTreeNode.deserialize(data)
+        i = 0
+        while i < node.pairs and key > node.keys[i]:
+            i += 1
+        if i < node.pairs and node.keys[i] == key:
+            return node.values[i]
+        if node.children[i] == 0:
+            return None
+        return self.searchBtree(node.children[i], key)
 
     def load(self):
         if not self.currentFile:
@@ -67,15 +179,49 @@ class BTreeIndex:
             for line in f:
                 try:
                     key, value = map(int, line.strip().split(','))
+                    self.insertKeyValue(key, value)
                     print(f"Loaded key={key}, value={value}.")
                 except ValueError:
-                    print("Error")
+                    print("Error in file format.")
 
-    def print_index(self):
+    def insertKeyValue(self, key, value):
+        self.currentFile.seek(0)
+        header = self.currentFile.read(24)
+        rootId = struct.unpack('>Q', header[8:16])[0]
+        if rootId == 0:
+            rootNode = BTreeNode(id=1)
+            rootNode.keys[0] = key
+            rootNode.values[0] = value
+            rootNode.pairs = 1
+            self.currentFile.seek(512)
+            self.currentFile.write(rootNode.serialize())
+            self.currentFile.seek(8)
+            self.currentFile.write(struct.pack('>Q', 1))
+        else:
+            self.insertToBtree(rootId, key, value)
+
+    def printIndex(self):
         if not self.currentFile:
             print("No file is currently open.")
             return
-        print("Printing all key/value pairs.")
+        self.currentFile.seek(0)
+        header = self.currentFile.read(24)
+        rootId = struct.unpack('>Q', header[8:16])[0]
+        if rootId == 0:
+            print("Tree is empty.")
+            return
+        self.printBtree(rootId)
+
+    def printBtree(self, id):
+        self.currentFile.seek(id * 512)
+        data = self.currentFile.read(512)
+        node = BTreeNode.deserialize(data)
+        for i in range(node.pairs):
+            if node.children[i] != 0:
+                self.printBtree(node.children[i])
+            print(f"Key: {node.keys[i]}, Value: {node.values[i]}")
+        if node.children[node.pairs] != 0:
+            self.printBtree(node.children[node.pairs])
 
     def extract(self):
         if not self.currentFile:
@@ -88,7 +234,25 @@ class BTreeIndex:
                 print("Quitting")
                 return
         with open(name, 'w') as f:
-            print(f"Extracted key/value pairs to '{name}'.")
+            self.currentFile.seek(0)
+            header = self.currentFile.read(24)
+            rootId = struct.unpack('>Q', header[8:16])[0]
+            if rootId == 0:
+                print("Tree is empty.")
+                return
+            self.extractBtree(rootId, f)
+        print(f"Extracted key/value pairs to '{name}'.")
+
+    def extractBtree(self, id, file):
+        self.currentFile.seek(id * 512)
+        data = self.currentFile.read(512)
+        node = BTreeNode.deserialize(data)
+        for i in range(node.pairs):
+            if node.children[i] != 0:
+                self.extractBtree(node.children[i], file)
+            file.write(f"{node.keys[i]},{node.values[i]}\n")
+        if node.children[node.pairs] != 0:
+            self.extractBtree(node.children[node.pairs], file)
 
     def quit(self):
         if self.currentFile:
@@ -103,7 +267,7 @@ class BTreeIndex:
             "insert": self.insert,
             "search": self.search,
             "load": self.load,
-            "print": self.print_index,
+            "print": self.printIndex,
             "extract": self.extract,
             "quit": self.quit,
         }
